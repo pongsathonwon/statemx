736b055cbfc0596738863726e515e8a37e1f0568
pongsathonwon <men1234-2008@hotmail.com>
2024-10-20 13:55:24 +0700

    setup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+import { FormEvent, useState } from "react";
+
+type NameState = {
+  firstname: string;
+  lastname: string;
+};
+
+const initState: NameState = {
+  firstname: "",
+  lastname: "",
+};
+
 const App = () => {
-  return <div>App</div>;
+  const [fullname, setFullname] = useState<NameState>(initState);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(fullname);
+  };
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col mx-auto w-1/2 p-4">
+      <label htmlFor="fName" className="flex justify-between p-1">
+        firstname
+        <input
+          className="border"
+          value={fullname.firstname}
+          onChange={({ target }) =>
+            setFullname((p) => ({ ...p, [target.name]: target.value }))
+          }
+          type="text"
+          id="fName"
+          name="firstname"
+          placeholder="firstname"
+        />
+      </label>
+      <label htmlFor="lName" className="flex justify-between p-1">
+        lastname
+        <input
+          className="border"
+          value={fullname.lastname}
+          onChange={({ target }) =>
+            setFullname((p) => ({ ...p, [target.name]: target.value }))
+          }
+          type="text"
+          id="lName"
+          name="lastname"
+          placeholder="lastname"
+        />
+      </label>
+      <button type="submit">submit</button>
+    </form>
+  );
 };
 
 export default App;
